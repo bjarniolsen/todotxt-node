c0dd4e8f5076540bf91b5703ece260701792b1f7
Bjarni Olsen <bjarniolsen@gmail.com>
2011-10-15 22:12:48 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var server = require("./lib/server");
+var router = require("./lib/router");
+var requestHandlers = require("./lib/requestHandlers");
+
+var handle =  {};
+handle['/'] = requestHandlers.show;
+handle['/todo'] = requestHandlers.show;
+handle['/add'] = requestHandlers.add;
+handle['/edit'] = requestHandlers.edit;
+
+server.start(router.route, handle);
